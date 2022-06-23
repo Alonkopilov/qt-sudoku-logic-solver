c9b9f6ee118450657e197c5c4cf107faf207c590
Alonk <alonkop12@gmail.com>
2022-06-23 15:15:55 +0300

    Added initial project code

---
+++ src/square.h
@@ -0,0 +1,26 @@
+#ifndef SQUARE_H
+#define SQUARE_H
+
+
+class Square
+{
+public:
+    Square();
+    Square(const int& digit, const bool& isPreset, const int& row, const int& col);
+    bool digitMarkupExists(const int& digit) const;
+    int getDigit() const;
+    void setDigit(const int& digit, const bool& isPreset);
+    void setMarkup(const int& digit);
+    void removeMarkup(const int& digit);
+    int getRow() const;
+    int getCol() const;
+
+private:
+    bool _markups[9];
+    int _digit;
+    bool _isPreset;
+    int _row;
+    int _col;
+};
+
+#endif // SQUARE_H
