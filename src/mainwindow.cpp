867166fc1abafe86ea4ae147b8c8d2462e468f10
Alonk <alonkop12@gmail.com>
2022-07-15 11:26:22 +0300

    Designed initial board ui

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,10 +6,49 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    generateBoard();
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::generateBoard()
+{
+    for (int i = 0; i < ui->glMainBoard->children().length(); i++)
+    {
+        QGridLayout* layout = (QGridLayout*)ui->glMainBoard->children()[i];
+
+        for (int j = 0; j < 9; j++) {
+            QLabel* num = new QLabel("0");
+            num->setAlignment(Qt::AlignCenter);
+            num->setStyleSheet("QLabel {color: #B4BBC4;}");
+
+            layout->addWidget(num, j/3, j%3);
+        }
+
+    }
+
+    //TESTING
+    QGridLayout* layoutTest = (QGridLayout*)ui->glMainBoard->children()[2];
+
+    if ( layoutTest != NULL )
+    {
+        QLayoutItem* item;
+        while ( ( item = layoutTest->takeAt( 0 ) ) != NULL )
+        {
+            delete item->widget();
+            delete item;
+        }
+    }
+
+    QLabel* num = new QLabel("0");
+    QFont* font = new QFont();
+    font->setPointSize(30);
+    num->setAlignment(Qt::AlignCenter);
+    num->setFont(*font);
+    layoutTest->addWidget(num);
 }
 
