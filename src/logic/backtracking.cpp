a50dff8906b5ed69de60053387d3ba4583f81461
alonkopilov <alonkop12@gmail.com>
2022-09-07 11:36:07 +0300

    Added the backtracking strategy

---
+++ src/logic/backtracking.cpp
@@ -0,0 +1,49 @@
+#include "backtracking.h"
+
+void Backtracking::doBacktracking(Board &board)
+{
+    Backtracking::solve(board);
+}
+
+bool Backtracking::solve(Board &board)
+{
+        int row = 0;
+        int col = 0;
+        std::pair<int,int> square = Backtracking::findEmpty(board);
+        if (square.first == 0 && square.second == 0)
+        {
+            return true;
+        }
+        else
+        {
+            row = square.first;
+            col = square.second;
+        }
+
+        for (int num = 1; num <= 9; num++)
+        {
+            if (board.checkSafe(board._squares[row][col], num))
+            {
+                board.setBoardDigit(row, col, num, false);
+                if (solve(board))
+                    return true;
+                board._squares[row][col].setDigit(0, false);
+            }
+        }
+        return false;
+}
+
+std::pair<int,int> Backtracking::findEmpty(Board &board)
+{
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            if (board.getBoardDigit(i, j) == 0)
+            {
+                return std::make_pair(i, j);
+            }
+        }
+    }
+    return std::make_pair(0,0);
+}
---
+++ src/logic/backtracking.h
@@ -0,0 +1,18 @@
+#ifndef BACKTRACKING_H
+#define BACKTRACKING_H
+
+#include "board.h"
+#include <bits/stdc++.h>
+
+class Board;
+class Backtracking
+{
+public:
+    static void doBacktracking(Board &board);
+
+private:
+    static bool solve(Board &board);
+    static std::pair<int,int> findEmpty(Board &board);
+};
+
+#endif // BACKTRACKING_H
