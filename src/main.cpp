9bf0090fd2b1a1e4943a9bca8658a5b3b5c06ece
Alonkopilov <alonkop12@gmail.com>
2023-03-12 22:15:05 +0200

    Added fonts

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,17 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
 
-    QFontDatabase::addApplicationFont("./fonts/Montserrat-Regular.ttf");
-    QFontDatabase::addApplicationFont("./fonts/Montserrat-Bold.ttf");
+    int res2 = QFontDatabase::addApplicationFont(":/fonts/fonts/Montserrat-Bold.ttf");
+    int res1 = QFontDatabase::addApplicationFont(":/fonts/fonts/Montserrat-Regular.ttf");
+
+
+
+    QFontDatabase db;
+     for(int i=0; i<db.families().size(); i++)
+     {
+       qDebug() << db.families().at(i);
+     }
+     std::cout << "res1 = " + std::to_string(res1) + ", res2 = " + std::to_string(res2) << std::endl;
 
     w.show();
     return a.exec();
